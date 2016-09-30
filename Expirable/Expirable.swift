8d7b00afaf549e17f0d9bb5a8d1805158710e059
Andrew Breckenridge <asbreckenridge@me.com>
2016-09-29 17:05:04 -0700

    add Expirable

---
+++ Expirable/Expirable.swift
@@ -0,0 +1,31 @@
+import Foundation
+import RxSwift
+
+protocol Expirable {
+    var expiresAt: Date { get }
+
+    /// If the token is going to expire in less than this, consider it expired and just get a new token
+    var expirationMarginInterval: TimeInterval { get }
+}
+
+extension Expirable {
+    var isValid: Bool { return expiresAt > Date() }
+
+    var isExpiredOrExpiringSoon: Bool {
+        let expiresIn = expiresAt.timeIntervalSinceNow
+        let expiredOrWillExpireSoon = expiresIn <= expirationMarginInterval
+
+        return expiredOrWillExpireSoon
+    }
+
+    var expiresSoon: Observable<Void> {
+        let expiresSoonAt = expiresAt.timeIntervalSinceNow - expirationMarginInterval
+
+        guard expiresSoonAt > 0 else {
+            return .just(()) // fire now if it expiresSoonAlready
+        }
+
+        return Observable<Int>.timer(expiresSoonAt, scheduler: MainScheduler.instance).map { _ in }
+    }
+
+}
